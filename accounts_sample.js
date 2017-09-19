f0abbd4c1497eb8b4efe34f2fb6e13b8b7abf79b
Jack <jackcodingarchive+github@gmail.com>
2017-09-19 04:15:36 -0500

    SetupTools preview release candidate 1

---
+++ accounts_sample.js
@@ -0,0 +1,40 @@
+// editor with syntax highlighting is recommended (for example, notepad++ or textmate)
+
+accounts = {
+
+// Put your data here as shown below. Don't forget the commas and quotes!
+// If your emails or passwords contain single quotes,
+// escape them with backslashes (\) like this: 'pass\'word'
+
+
+'email': 'pass',
+'email2': 'pass2',
+
+
+};// don't delete this line!
+
+
+// how many characters are displayed in each row (within one account)
+rowlength = 7;
+
+// change to 1 to switch to testing
+testing = 0;
+
+// change to 1 to enable price display in tooltips
+prices = 0;
+
+// change to 1 to enable one-click login (run lib/mulelogin.au3 first)
+mulelogin = 0;
+
+// 0 = use smart layout (fill empty spaces)
+// 1 = show account boxes row by row
+nomasonry = 0;
+
+//  seconds to delay between loading separate accounts
+accountLoadDelay = 2;
+
+//  whether or not to enable debug console logging
+debugging = false;
+
+//  if you have your own imgur client id then uncomment this line and add it here; this is not a required setting
+//ImgurClientID = '';
